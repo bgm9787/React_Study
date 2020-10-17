f267eda81d88dc4b2123796ea65242d9c60acb45
baoguomei <bgm9787@163.com>
2020-10-17 21:05:21 +0800

    feat: 🎸 learn scaffold

--- 03_learn_scaffold/src/App.js
+++ 03_learn_scaffold/src/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// yarn.lock文件：记录项目中各个module真实的安装版本
+// package.json文件：整个项目的配置信息
+// 项目启动：yarn start
+
+// manifest.json文件 & serviceWorker.js文件
+// ------> WA技术
+// 对网页进行安装和离线缓存
+
+// yarn eject命名
+// ------> 把webpack中的配置暴露出来
 function App() {
   return (
     <div className="App">
